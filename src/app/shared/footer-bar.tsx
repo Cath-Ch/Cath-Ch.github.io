ad68ec8f839c3cc8c694f5d38396acef6e460c13
Catherine Chen <cchen366@uottawa.ca>
2024-01-24 21:57:54 -0500

    Started header

---
+++ src/app/shared/footer-bar.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+
+export default function FooterBar() {
+    return (
+        <AppBar position="static">
+
+        </AppBar>
+    );
+}
